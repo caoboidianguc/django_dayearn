f02c9fc289c7b0aa83fdbca515b91c87b8ce0476
cao_boi_dia_nguc <jubivu@icloud.com>
2025-02-26 19:10:36 -0500

    switch to postgres

--- dayearn/settings.py
+++ dayearn/settings.py
@@ -91,13 +91,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dayearn.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dayearn_db',
+        'USER': 'postgres',
+        'PASSWORD': '7818',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
