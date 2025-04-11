c3a8e483711fa5fb0705b9428bd8e65dd641ecdd
cao_boi_dia_nguc <jubivu@icloud.com>
2025-04-10 23:37:57 -0400

    vacation notify in view

--- ledger/views.py
+++ ledger/views.py
@@ -455,7 +455,8 @@ class ClientWalkinView(LoginRequiredMixin, CreateView):
             )
         khach.services.set(dv)    
         form.instance = khach
-        saveKhachVisit(khach, ngay,thoigian,dv, khach.technician, KhachVisit.Status.anyone)
+        if not khach.future_visit:
+            saveKhachVisit(khach, ngay,thoigian,dv, khach.technician, KhachVisit.Status.anyone)
         messages.success(self.request, f"Welcom {form.instance.full_name} to our salon!")
         return super().form_valid(form)
 
